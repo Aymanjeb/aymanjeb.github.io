f905d0a5d87f057a27c6a3caa27fe58167a88720
Aymanjeb <aymanjeb02@gmail.com>
2024-07-08 22:22:57 +0200

    Test lecture pdf

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,11 @@ $(function() {
 });
 
 
+function toggleParagraph(element) {
+  const content = element.nextElementSibling;
+  if (content.style.display === "none" || content.style.display === "") {
+      content.style.display = "block";
+  } else {
+      content.style.display = "none";
+  }
+}
